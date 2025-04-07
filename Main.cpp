cf119638df2f99eaa6af4d1ce718af001f1afd21
ihages <hagemaij@mail.uc.edu>
2025-04-07 16:47:24 -0400

    filled main sequence with placeholders, got the test data array done and it prints when the test case isnt commented out

--- Main.cpp
+++ Main.cpp
@@ -15,6 +15,7 @@ HashTable2D TwoDHash;
 
 int main() {
 	//generate 100 random unique intergers
+	std::string placeholder = "This is a placeholder for now";
 
 	int randomSeed = 0;
 	srand(randomSeed);
@@ -45,22 +46,36 @@ int main() {
 		TestNumsArr[i] = AscendPointer[i];
 		std::cout << TestNumsArr[i] << std::endl;
 	}
-	std::cout << "Numbers generated: "<<numCount << std::endl;
+	std::cout << "Numbers generated: "<< numCount << std::endl;
 		
 	//insert the first 50 value in both structures. 
 	//	KEEP TRACK of how many slots you have to check--then record this value (how?)
-	//for (int i = 0; i < 50; i++) {
-		//TwoDHash.Insert(TestNumsArr[i]);
-		//OneDHash.Insert(TestNumsArr[i]);
-		/*
-		* This is all kinda a placeholder for now. I am not quite sure if having i as a parameter for find will work
-		*/
-	//}
-	//REmove all items from the data structure where the index of the value in the main function % 7 = 0.
-	//	KEEP TRACK of how many slots you have to check--then record this value (how?)
+	for (int i = 0; i < 50; i++) {
+		TwoDHash.Insert(TestNumsArr[i]);
+		OneDHash.Insert(TestNumsArr[i]);
+		std::cout << "Added " << TestNumsArr[i] << " to the data structure." << std::endl;
+		//This is all kinda a placeholder for now. I am not quite sure if having i as a parameter for find will work
+	}
+	std::cout << "Number of slots checked to insert: " << placeholder << std::endl;
 	
-	//Insert the remaining 50 values from both data structures. record the running number of spots checked to remove
+	//Remove all items from the data structure where the index of the value in the main function % 7 = 0.
+	//	KEEP TRACK of how many slots you have to check--then record this value (how?)
+	for (int i = 0; i < 50; i++) {
+		if (i % 7 == 0) {
+			TwoDHash.Remove(TestNumsArr[i]);
+			OneDHash.Remove(TestNumsArr[i]);
+			std::cout << "Removed " << TestNumsArr[i] << " from the data structure." << std::endl;
+		}
+	}
+	std::cout << "Number of slots checked to delete: " << placeholder << std::endl;
 
+	//Insert the remaining 50 values from both data structures. record the running number of spots checked to remove
+	for (int = 50; i < 100; i++) {
+		TwoDHash.Insert(TestNumsArr[i]);
+		OneDHash.Insert(TestNumsArr[i]);
+		std::cout << "Added " << TestNumsArr[i] << " to the data structure." << std::endl;
+	}
+	std::cout << "Number of slots checked to insert: " << placeholder << std::endl;
 
 	return 0;
 }
