5b8759eec569e332c74b1a5ce0098b29c0f44c25
ihages <hagemaij@mail.uc.edu>
2025-04-07 16:19:47 -0400

    fixed another error i made

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <string>
 #include <random>
@@ -51,52 +50,3 @@ int main() {
 
 	return 0;
 }
-=======
-#include <iostream>
-#include <string>
-#include <random>
-
-#include"Node.h"
-#include"Exceptions.h"
-#include"BinaryTree.h"
-#include"2DHashTable.h"
-#include"HashTable1D.h"
-
-BinaryTree<int> TestNums;
-HashTable2D TwoDHash;
-
-
-int main() {
-	//generate 100 random unique intergers
-
-	int randomSeed = 0;
-	srand(randomSeed);
-	for (int i = 0; i < 100; i++) {
-		int newNum = rand();
-		if (TestNums.Find(newNum, TestNums.getRoot()) == -1) {
-			//if the number isnt in the list
-			//insert the number into the list
-			TestNums.Insert(newNum, TestNums.getRoot());
-		}
-		else {
-			//if the number is in the list
-			//do nothing
-			while (TestNums.Find(newNum, TestNums.getRoot()) != -1) {
-				newNum = rand(); //generate a new number
-			}
-			TestNums.Insert(newNum, TestNums.getRoot());
-		}
-	}
-		
-	//insert the first 50 value in both structures. 
-	//	KEEP TRACK of how many slots you have to check--then record this value (how?)
-	
-	//REmove all items from the data structure where the index of the value in the main function % 7 = 0.
-	//	KEEP TRACK of how many slots you have to check--then record this value (how?)
-	
-	//Insert the remaining 50 values from both data structures. record the running number of spots checked to remove
-
-
-	return 0;
-}
-
